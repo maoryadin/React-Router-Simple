4498ee7c84e8b39c7f37cc3edcc280be8605f315
maor yadin <31710467+maoryadin@users.noreply.github.com>
2023-06-10 12:20:52 +0000

    Update 3 files

--- src/navigation-router.js
+++ src/navigation-router.js
@@ -1,6 +1,21 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const RouterContext = createContext();
+const CreateRouterContext = () => {
+  const context = createContext(undefined);
+
+  const useRoute = () => {
+    const contextValue = useContext(context);
+
+    if (contextValue === undefined) {
+      throw new Error("useRoute must be used within a RouteProvider");
+    }
+    return contextValue;
+  };
+
+  return [useRoute, context.Provider];
+};
+
+const [useRoute, RouterContext] = CreateRouterContext();
 
 export const Router = ({ children }) => {
   // define the route.
@@ -10,21 +25,17 @@ export const Router = ({ children }) => {
     window.history.replaceState(null, null, route);
   }, [route]);
 
-  return (
-    <RouterContext.Provider value={{ route, setRoute }}>
-      {children}
-    </RouterContext.Provider>
-  );
+  return <RouterContext value={{ route, setRoute }}>{children}</RouterContext>;
 };
 
 export const Link = ({ to, children }) => {
-  const { setRoute } = useContext(RouterContext);
+  const { setRoute } = useRoute(RouterContext);
 
   return <div onClick={() => setRoute(to)}>{children}</div>;
 };
 
 export const Route = ({ path, children }) => {
-  const { route } = useContext(RouterContext);
+  const { route } = useRoute(RouterContext);
 
   return route === path ? children : null;
 };
